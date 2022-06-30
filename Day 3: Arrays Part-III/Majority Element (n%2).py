d72bf1ddc89e604af8abab9675318aeb12396249
Manas Ranjan Munda <64586228+manas1410@users.noreply.github.com>
2022-06-30 16:12:56 +0530

    Create Majority Element (n%2).py

---
+++ Day 3: Arrays Part-III/Majority Element (n%2).py
@@ -0,0 +1,14 @@
+'''
+problem url :-https://leetcode.com/problems/majority-element/
+'''
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        '''if n<0:
+            return self.myPow(1/x,-1*n)
+        if n==1:
+            return x
+        if n>1 and n%2==0:
+            num = self.myPow(x,n//2)
+            return num * num
+        return x*self.myPow(x,n-1)'''
